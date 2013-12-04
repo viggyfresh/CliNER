75337711640c69f5daf8bf0dc971d097c043f62a
Willie Boag <wboag@dumuzi.(none)>
2013-12-04 00:52:50 -0500

    Moved functionality from helper functions to Note class. Also, added support for plain format and attempted xml

--- code/note.py
+++ code/note.py
@@ -1,119 +1,245 @@
-"""Internal note representation.
-
-Note class reads in i2b2 formatted .txt and .con files and aligns them. After-
-ward they can be accessed by iterating over the class.
-
-Three auxiliary methods, read_txt(), read_con(), and write_con() streamline 
-help manipulate such files as well.
-"""
-# TODO: clean up interface
-# TODO: add support for non-i2b2 formats
-
 from __future__ import with_statement
 
+import re
 
-# This is a test
 
 class Note:
-	"""Note representation.
 
-	Notes are represented as an iterable of tuples (word, concept).
-	"""
-	# TODO: more efficient internal representation
+    # Constructor
+    def __init__(self):
+	# data     - A list of lines directly from the file
+	# concepts - A one-to-one correspondence of each word's concept
+        self.data     = []
+        self.concepts = []
 
-	def __init__(self, txt, con=None):
-		"""Read in the note from file(s).
 
-		Args:
-			txt: File containing i2b2-formatted text.
-			con: File containing i2b2-formatted concepts.
-		"""
 
-		# Read in list of tokens, each with "none" concept
-		self.sents = []
-		with open(txt) as f:
-			for line in f:
-				self.sents.append([[w, "none"] for w in line.split()])
-		
-		# Assign appropriate concept if a concept file was given
-		if con:
-			with open(con) as f:
-				for line in f:
-					c, t = line.split('||')
-					t = t[3:-2]
-					c = c.split()
-					start = c[-2].split(':')
-					end = c[-1].split(':')
-					assert "concept spans one line", start[0] == end[0]
-					l = int(start[0]) - 1
-					start = int(start[1])
-					end = int(end[1])
-					
-					for i in range(start, end + 1):
-						self.sents[l][i][1] = t
+    # Note::read_i2b2()
+    #
+    # @param txt. A file path for the i2b2 tokenized medical record
+    # @param con. A file path for the i2b2 annotated concepts associated with txt
+    def read_i2b2(self, txt, con=None):
 
-	def __iter__(self):
-		"""Yield the (word, concept) pairs."""
-		return iter(self.sents)
-		
-def read_txt(txt):
-	"""Get a list of words from an i2b2-formatted text file.
-
-	Ags:
-		txt: The i2b2-formatted text file.
-
-	Returns:
-		A list of the words in the text file.
-	"""
-	# TODO: this method is misleading, should include concept for consistency
-
-	note = []
+        # Read in the medical text
 	with open(txt) as f:
+	    for line in f:
+		# Add sentence to the data list
+                self.data.append(line)
+
+		# For each word, store a corresponding concept label
+		tmp = []
+		for word in line.split():
+		    tmp.append('none')
+		self.concepts.append(tmp)
+
+
+
+        # If an accompanying concept file was specified, read it
+	if con:
+	    with open(con) as f:
 		for line in f:
-			note.append([w for w in line.split()])
-	return note		
-	
-def read_con(con, txt):
-	"""Get a list of (word, concept) pairs from a txt and con file.
+		    c, t = line.split('||')
+		    t = t[3:-2]
+		    c = c.split()
+		    start = c[-2].split(':')
+		    end = c[-1].split(':')
+		    assert "concept spans one line", start[0] == end[0]
+		    l = int(start[0]) - 1
+		    start = int(start[1])
+		    end = int(end[1])
 
-	Args:
-		con: File containing the i2b2-formatted concepts.
-		txt: File containing the i2b2-formatted text.
+		    for i in range(start, end + 1):
+			self.concepts[l][i] = t
 
-	Returns:
-		A list of (word, concept) pairs.
-	"""
-	# TODO: this method obviates the Note class, should complement
 
-	label = [['none'] * len(line) for line in txt]
-	with open(con) as f:
-		for line in f:
-			c, t = line.split('||')
-			t = t[3:-2]
-			c = c.split()
-			start = c[-2].split(':')
-			end = c[-1].split(':')
-			assert "concept spans one line", start[0] == end[0]
-			l = int(start[0]) - 1
-			start = int(start[1])
-			end = int(end[1])
-			
-			for i in range(start, end + 1):
-				label[l][i] = t
-	return label
-		
-def write_con(con, data, labels):
-	"""Writes concept file.
 
-	From data and labels, this create a .con file as output.
-	"""
-	# TODO: method should call a serialization primitive of Note class
+    # Note::write_i2b2()
+    #
+    # @param  con.    A path to the file of where to write the prediction.
+    # @param  labels. A list of predictions of labels for the given text.
+    #
+    # Write the concept predictions to a given file in i2b2 format
+    def write_i2b2(self, con, labels):
 
 	with open(con, 'w') as f:
-		for i, tmp in enumerate(zip(data, labels)):
-			datum, label = tmp
-			for j, tmp in enumerate(zip(datum, label)):
-				datum, label = tmp
-				if label != 'none':
-					idx = "%d:%d" % (i + 1, j)
-					print >>f, "c=\"%s\" %s %s||t=\"%s\"" % (datum, idx, idx, label)
+	    for i, tmp in enumerate(zip(self.txtlist(), labels)):
+		datum, label = tmp
+		for j, tmp in enumerate(zip(datum, label)):
+		    datum, label = tmp
+		    if label != 'none':
+			idx = "%d:%d" % (i + 1, j)
+			print >>f, "c=\"%s\" %s %s||t=\"%s\"" % (datum, idx, idx, label)
+
+
+
+    # Note::read_plain()
+    #
+    # @param txt. A file path for the plain tokenized medical record
+    # @param con. A file path for the annotated concepts associated with txt
+    def read_plain(self, txt, con=None):
+
+        # Read in the medical text
+	with open(txt) as f:
+
+            for line in f:
+		# Add sentence to the data list
+		self.data.append(line)
+
+		# For each word, store a corresponding concept label
+		tmp = []
+		for word in line.split():
+		    tmp.append('none')
+		self.concepts.append(tmp)
+
+
+        # If an accompanying concept file was specified, read it
+	if con:
+	    with open(con) as f:
+		for line in f:
+		    c, t = line.split('||')
+		    t = t[3:-2]
+		    c = c.split()
+
+		    start = c[-2].split(':')
+		    end = c[-1].split(':')
+
+		    assert "concept spans one line", start[0] == end[0]
+		    l = int(start[0]) - 1
+		    start = int(start[1])
+		    end = int(end[1])
+
+                    # Tokenize the input intervals
+                    stok = len(self.data[l][:start].split())
+                    etok = len(self.data[l][start:end+1].split()) + stok - 1
+
+                    # Update the corresponding concept labels
+		    for i in range(stok, etok + 1):
+	               self.concepts[l][i] = t
+
+
+
+    # Note::write_plain()
+    #
+    # @param  con.    A path to the file of where to write the prediction.
+    # @param  labels. A list of predictions of labels for the given text.
+    #
+    # Write the concept predictions to a given file in plain format
+    def write_plain(self, con, labels):
+
+        with open(con, 'w') as f:
+
+            # Search every token
+            for i, line in enumerate(labels):
+                for j, label in enumerate(line):
+
+                    # Only print non-trivial tokens
+                    if label != 'none':
+                        
+                        words = self.data[i].split()
+
+                        # Get the untokenized starting index
+                        start = 0
+                        for k in range(j):
+                            start += len(words[k]) + 1
+
+                        # Untokenized ending index
+                        end = start + len(words[j]) - 1
+
+                        # Format the starting and ending indices
+                        sidx = "%d:%d" % (i + 1, start)
+                        eidx = "%d:%d" % (i + 1, end)
+
+                        # Get the string stored in the plaintext representation
+                        datum = self.data[i][start:end+1]
+
+                        print >>f, "c=\"%s\" %s %s||t=\"%s\"" % (datum, sidx, eidx, label)
+
+
+
+    # Note::read_xml()
+    #
+    # @param txt. A file path for the xml formatted medical record
+    def read_xml(self, txt):
+
+       # FIXME: By storing the 'data' as a list of lines from the file
+       #          instead of a list of list of words, xml does not fit nicely
+       # Possible solution: Switch back to list of list of words
+       # Alternative:       Store edited sentences from the file to remove <>
+
+
+        # Read in the medical text
+        with open(txt) as f:
+
+            for line in f:
+                # Add sentence to the data list
+                self.data.append(line)
+
+
+                # For each word, store a corresponding concept label
+                tmp = []
+                for i, group in enumerate(line.split('<')):
+                    # All odd groups have label info (because of line split)
+		    # ex. 'treatment>discharge medications'
+                    if (i%2):
+                        # Get concept label
+                        match = re.search('(\w+)>(.+)', group)
+                        if not match: 
+                            print "\nUnexpected file format\n"
+                            exit(1)
+
+                        label = match.group(1)
+                        words = match.group(2)
+                       
+                        # Add the label once for every word
+                        for word in words.split():
+                            tmp.append(label)
+                    # If even group , then process with 'none' labels
+                    else:
+                        for word in group.split():
+			    # / closes the xml tag of a previous label (skip)
+                            if word[0] == '/':
+                                continue
+                            else:
+                                tmp.append('none')
+                # Add line of labels to the 'concepts' data member
+                self.concepts.append(tmp)
+
+
+
+    # Note::write_xml()
+    #
+    # @param  con.    A path to the file of where to write the prediction.
+    # @param  labels. A list of predictions of labels for the given text.
+    #
+    # Write the concept predictions to a given file in xml format
+    def write_xml(self, con, labels):
+        # xml formats do not have associated concept files
+        return
+
+
+
+
+    # txtlist()
+    #
+    # @return the data from the medical text broken into line-word format
+    def txtlist( self ):
+        # Goal: Break self.data sentences into lists of words
+        ans = []
+        for sent in self.data:
+            ans.append(sent.split())
+
+        return ans
+
+
+
+    # conlist()
+    #
+    # @return a list of lists of the concepts associated with each word from data
+    def conlist( self ):
+	return self.concepts
+
+
+
+    # For iterating
+    def __iter__(self):
+	return iter(self.data)
