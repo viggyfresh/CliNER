39f05914c2bef7228d3d33afb48245b9d5abff9c
Willie Boag <wboag@dumuzi.(none)>
2014-04-01 18:47:31 -0400

    added support for cui and hypernym lookups

--- code/SQLookup.py
+++ code/SQLookup.py
@@ -26,13 +26,45 @@ c = SQLConnect()
 def SQlookup( c , string ):
 
     #queries database and finds first semantic type match, returns a 1 when a match is found. 
-    c.execute( "SELECT sty FROM MRCON a, MRSTY b WHERE a.cui = b.cui AND str = ? LIMIT 1;" , (string,) )
+    c.execute( "SELECT sty FROM MRCON a, MRSTY b WHERE a.cui = b.cui AND str = ?; " , (string,) )
 
     #returns a tuple with the match or None if there was  no match.  
-    return c.fetchone() 
+    #return c.fetchone() 
+    return c.fetchall() 
 
 #returns the semantic type of a word 
 def string_lookup( string ):
     
-    return SQlookup( c ,  string )
+    #return SQlookup( c ,  string )
+    r = SQlookup( c ,  string )
+
+    return r
+
+
+# get all semantic types for a given concept
+def cui_lookup( string ):
+
+    # queries database and finds semantic type match
+    c.execute( "SELECT cui FROM MRCON WHERE str = ?;" , (string,) )
+
+    #returns a tuple with the match or None if there was  no match.  
+    return c.fetchall() 
+
+
+def hypernyms_lookup( string ):
+
+    c.execute( "SELECT CUI FROM MRCON WHERE STR = ? LIMIT 1 ;" , (string,) )
+
+    cui = c.fetchone()
+
+    if cui == None:
+        return None
+    else:
+        c.execute( "SELECT CUI2 FROM MRREL WHERE CUI1 = ? AND REL = 'PAR' LIMIT 5 ;" , (cui[0],) )
+        result = c.fetchone()
+        if not result:
+            return None
+        else:
+            return result
+
 
