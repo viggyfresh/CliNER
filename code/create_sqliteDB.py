097b4d3594da8040e3f98b53a298a53029395007
Willie Boag <wboag@dumuzi.(none)>
2014-04-01 18:49:25 -0400

    added MRREL table for hypernyms

--- code/create_sqliteDB.py
+++ code/create_sqliteDB.py
@@ -10,6 +10,7 @@ def create_db():
     
     conn.text_factory = str 
 
+    print "opening files" 
     #load data in files.
     try:
         MRSTY_TABLE = open( ( "../umls_tables/MRSTY"), "r" )
@@ -25,27 +26,49 @@ def create_db():
         conn.close() 
         sys.exit() 
 
-    MRSTY_TABLE = MRSTY_TABLE.read() ;
+    try:
+        MRREL_TABLE = open( ( "../umls_tables/MRREL") , "r" )
+    except IOError:
+        print "\nNo file to use for creating MRREL table\n"
+        conn.close()
+        sys.exit()
+
+    print "reading files"
+
+    MRSTY_TABLE = MRSTY_TABLE.read() 
     MRSTY_TABLE = MRSTY_TABLE.split('\n')
 
-    MRCON_TABLE = MRCON_TABLE.read() ; 
+    MRCON_TABLE = MRCON_TABLE.read()  
     MRCON_TABLE = MRCON_TABLE.split( '\n' ) 
+
+    MRREL_TABLE = MRREL_TABLE.read() 
+    MRREL_TABLE = MRREL_TABLE.split( '\n' ) 
 
     #data that will be inserted into tables. 
     MRTSY_DATA = [] 
     MRCON_DATA = [] 
+    MRREL_DATA = [] 
 
     c = conn.cursor() 
+
+    print "parsing files" 
 
     #parse and store the data from the files. 
     for line in MRSTY_TABLE: 
         MRTSY_DATA.append( tuple(line.split('|')) )
     for line in MRCON_TABLE:
         MRCON_DATA.append( tuple(line.split('|')) )
+    for line in MRREL_TABLE:
+        MRREL_DATA.append( tuple(line.split('|')) )
+
+    print "creating tables" 
 
     #create tables. 
     c.execute( "CREATE TABLE MRCON( CUI, LAT, TS, LUI, STT, SUI, STR, LRL, EMPTY ) ;" )
     c.execute( "CREATE TABLE MRSTY( CUI, TUI, STY, EMPTY ) ;" )  
+    c.execute( "CREATE TABLE MRREL( CUI1, REL, CUI2, RELA, SAB, SL, MG, EMPTY ) ;" ) 
+
+    print "inserting data" 
 
     #insert data onto database
     for line in MRCON_DATA:
@@ -58,11 +81,21 @@ def create_db():
             c.execute( "INSERT INTO MRSTY( CUI, TUI, STY, EMPTY) values( ?, ?, ?, ?)" , line )  
         except sqlite3.ProgrammingError:
             continue 
+    for line in MRREL_DATA:
+        try:
+            c.execute( "INSERT INTO MRREL(  CUI1, REL, CUI2, RELA, SAB, SL, MG, EMPTY ) values( ?, ?, ?, ?,?, ? ,? ,? )" , line )
+        except sqlite3.ProgrammingError:
+            continue 
+
+    print "creating indices" 
 
     #create indices for faster queries
     c.execute( "CREATE INDEX mrsty_cui_map ON MRSTY(CUI)")  
     c.execute( "CREATE INDEX mrcon_str_map ON MRCON(STR)")  
     c.execute( "CREATE INDEX mrcon_cui_map ON MRCON(CUI)")
+    c.execute( "CREATE INDEX mrrel_cui2_map ON MRREL( CUI2 )" )
+    c.execute( "CREATE INDEX mrrel_cui1_map on MRREL( CUI1 ) " ) 
+    c.execute( "CREATE INDEX mrrel_rel_map on MRREL( REL )" ) 
 
     #save changes to .db
     conn.commit()
