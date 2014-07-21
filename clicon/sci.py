c9920e79d1399ee1eefaa4fd6de7307779359e75
Willie Boag <wboag@dumuzi.(none)>
2014-07-21 18:10:38 -0400

    Moved vectorizers and classifiers into model.py

--- clicon/sci.py
+++ clicon/sci.py
@@ -19,22 +19,8 @@ def bits(n):
         yield b
         n ^= b
 
-'''
-def write_features( model_filename, rows, labels=None ):
 
-    file_suffix = ".sci" 
-    if labels is None:
-        file_suffix += ".test.in"
-    filename = model_filename + file_suffix
-    with open(filename, "w") as f:                                    
-        f.write( str(rows) + '\n' )
-        if labels is not None:
-            assert "Dimension mismatch", len(rows) == len(labels)
-        f.write( str(labels) ) 
-    f.close() 
-'''
-
-def train(X, Y, type):
+def train(X, Y, type, do_grid):
 
     # Search space
     C_range     = 10.0 ** np.arange( -5, 9 )
@@ -45,69 +31,45 @@ def train(X, Y, type):
     retVal = {}
     for t in bits(type):
 
-        if t == SVM:
-            estimates = SVC()
-            gamma_range = 10.0 ** np.arange( -5 , 9 )
-            parameters = [{'kernel':['rbf'], 'C':C_range, 'gamma':gamma_range}]    
-            
-        if t == LIN:
-            estimates = LinearSVC() 
-            parameters = [ {'C':C_range } ]
-            
-        # Find best classifier
-        clf = GridSearchCV(estimates, parameters, score_func = f1_score, n_jobs = cpu_count() )
-        clf.fit( X , Y )
-        retVal[t] = clf
+        if do_grid:
+
+            if t == SVM:
+                estimates = SVC()
+                gamma_range = 10.0 ** np.arange( -5 , 9 )
+                parameters = [{'kernel':['rbf'], 'C':C_range, 'gamma':gamma_range}]    
+                
+            if t == LIN:
+                estimates = LinearSVC() 
+                parameters = [ {'C':C_range } ]
+                
+            # Find best classifier
+            clf = GridSearchCV(estimates, parameters, score_func = f1_score, n_jobs = cpu_count() )
+            clf.fit( X , Y )
+            retVal[t] = clf
+
+        else:
+
+            clf = LinearSVC()
+            clf.fit(X, Y)
+            retVal[t] = clf
 
 
     # Return all chosen classifiers
-    return clf
+    return retVal
 
 
 
-def predict(model_filename, type):
-    filename = model_filename + ".sci"
-    X = [] 
-    with open(filename + ".test.in", "rd") as f:
-                X = literal_eval(f.readline().strip())
-    f.close()
-    
-    vec = pickle.load( open( filename + ".vec" ,"rd" ) )
+def predict(clfs, X, type):
 
-    print "Vectorizing"
-        #i don't like the transform here, but itll have to do for now.                                                                                                          
-    X = vec.transform(X).toarray()
-    X = np.array( X , object )
+    retVal = {}
 
     for t in bits(type):
-        print "Performing Grid Search"
-        if t == SVM:
-            clf = pickle.load( open( filename + ".svm.trained" , "rd" ) )
-            predict_input = filename + ".svm.test.out"
+        # Predict
+        clf = clfs[t]
+        output = clf.predict(X)
+        retVal[t] = output
 
-        if t == LIN:
-            clf = pickle.load( open( filename + ".lin.trained" , "rd" ) ) 
-            predict_input = filename + ".lin.test.out"
-    #i don't like the transform here, but itll have to do for now. 
-    
-        with open( predict_input, "w") as prediction:
-            for label in clf.predict(X):
-                prediction.write( str(label) + '\n' ) 
-        prediction.close()
-
-def read_labels(model_filename, type ):
-    labels = {} 
-    for t in bits(type):
-        if t == SVM:
-            filename = model_filename + ".sci.svm.test.out"
-        if t == LIN:
-            filename = model_filename + ".sci.lin.test.out"
-
-        with open(filename) as f:
-            lines = f.readlines()                                                                                                         
-        labels[t] = [line.strip() for line in lines]
-
-        return labels
+    # Return all predictions
+    return retVal
 
 
-    
