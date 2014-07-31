07259abf00ea29ac496cc83583f0bbc0105cdbac
Willie Boag <wboag@dumuzi.(none)>
2014-07-31 12:59:06 -0400

    Reorganized structure of features directory

--- clicon/features/features.py
+++ clicon/features/features.py
@@ -1,233 +1,273 @@
+######################################################################
+#  CliCon - clicon_features.py                                       #
+#                                                                    #
+#  Willie Boag                                      wboag@cs.uml.edu #
+#                                                                    #
+#  Purpose: Isolate the model's features from model.py               #
+######################################################################
+
+
+__author__ = 'Willie Boag'
+__date__   = 'Jan. 27, 2014'
+
+
+
 import nltk
+import re
+from wordshape import getWordShapes
 
-from sets import ImmutableSet
-
-from wordshape import *
-
-sentence_features = ImmutableSet(["pos", "stem_wordnet", "test_result", "prev", "next"])
-word_features = ImmutableSet(["word", "length", "mitre", "stem_porter", "stem_lancaster", "word_shape"])
-enabled_features = sentence_features | word_features
-
-    # input:  A sentence from a medical text file (list of words)
-    # output: A list of hash tables
-def features_for_sentence(sentence):
-    features_list = []
-
-    for word in sentence:
-        features_list.append(features_for_word(word))
-
-    tags = None
-    for feature in sentence_features:
-        if feature not in enabled_features:
-            continue
-
-        if feature == "pos":
-            tags = tags or nltk.pos_tag(sentence)
-            for i, features in enumerate(features_list):
-                tag = tags[i][1]
-                features[(feature, tag)] = 1
-
-        if feature == "stem_wordnet":
-            tags = tags or nltk.pos_tag(sentence)
-            morphy_tags = {
-                'NN': nltk.corpus.reader.wordnet.NOUN,
-                'JJ': nltk.corpus.reader.wordnet.ADJ,
-                'VB': nltk.corpus.reader.wordnet.VERB,
-                'RB': nltk.corpus.reader.wordnet.ADV}
-            morphy_tags = [(w, morphy_tags.setdefault(t[:2], nltk.corpus.reader.wordnet.NOUN)) for w, t in tags]
-            st = nltk.stem.WordNetLemmatizer()
-            for i, features in enumerate(features_list):
-                tag = morphy_tags[i]
-                features[(feature, st.lemmatize(*tag))] = 1
-
-        if feature == "test_result":
-            for index, features in enumerate(features_list):
-                right = " ".join([w for w in sentence[index:]])
-                if is_test_result(right):
-                    features[(feature, None)] = 1
-
-
-    ngram_features = [{} for i in range(len(features_list))]
-    if "prev" in enabled_features:
-        prev = lambda f: {("prev_"+k[0], k[1]): v for k, v in f.items()}
-        prev_list = map(prev, features_list)
-        for i in range(len(features_list)):
-            if i == 0:
-                ngram_features[i][("prev", "*")] = 1
-            else:
-                ngram_features[i].update(prev_list[i-1])
-
-    if "next" in enabled_features:
-        next = lambda f: {("next_"+k[0], k[1]): v for k, v in f.items()}
-        next_list = map(next, features_list)
-        for i in range(len(features_list)):
-            if i == len(features_list) - 1:
-                ngram_features[i][("next", "*")] = 1
-            else:
-                ngram_features[i].update(next_list[i+1])
-
-    merged = lambda d1, d2: dict(d1.items() + d2.items())
-    features_list = [merged(features_list[i], ngram_features[i])
-        for i in range(len(features_list))]
-
-    return features_list
+from sentence_features import SentenceFeatures
 
 
 
-    # input:  a single word, like
-    #         Admission
+class FeatureWrapper:
+
+    # FIXME - Make three objects - one for each classifier
+
+
+    # Instantiate an FeatureWrapper object
+    def __init__(self, data=None):
+
+        # Sentence-level features
+        self.feat_sent = SentenceFeatures(data)
+
+
+
+    # IOB_features()
+    #
+    # input:  A sentence
     # output: A hash table of features
-    #         features include: word, length, mitre, stem_porter
-def features_for_word( word):
-    features = {'dummy': 1}  # always have >0 dimensions
+    def IOB_features(self, sentence):
 
-            # word_shape, word, length, mitre, stem_porter, stem_lancaster
-    for feature in word_features:
+        # Different features depending on whether sentence is 'prose'
+        isProse = self.prose_sentence(sentence)
 
-                    # word_shape, test_result, word, pos, next, length, stem_wordnet, mitre, stem_porter, prev, stem_lancaster
-        if feature not in enabled_features:
-            continue
+        if isProse:
+            features_list = self.feat_sent.IOB_prose_features(sentence)
+        else:
+            features_list = self.feat_sent.IOB_nonprose_features(sentence)
 
-        if feature == "word":
-            features[(feature, word)] = 1
+        # Return features as well as indication of whether it is prose or not
+        return (isProse, features_list)
 
-        if feature == "length":
-            features[(feature, None)] = len(word)
 
-        if feature == "mitre":
-            for f in mitre_features:
-                if re.search(mitre_features[f], word):
-                    features[(feature, f)] = 1
 
-        if feature == "stem_porter":
-            st = nltk.stem.PorterStemmer()
-            features[(feature, st.stem(word))] = 1
+    # concept_features()
+    #
+    # input:  A sentence/line from a medical text file (list of chunks)
+    #         An list of indices into the sentence for each important chunk
+    # output: A list of hash tables of features
+    def concept_features(self, sentence, chunk_inds):
+        
+        # FIXME - move all of this work to SentenceFeatures object
 
-        if feature == "stem_lancaster":
-            st = nltk.stem.LancasterStemmer()
-            features[(feature, st.stem(word))] = 1
+        features_list = [ {} for _ in chunk_inds ]
 
-        if feature == "stem_snowball":
-            st = nltk.stem.SnowballStemmer("english")
-            #features[(feature, st.stem(word))] = 1
 
-        if feature == "word_shape":
-            wordShapes = getWordShapes(word)
-            for i, shape in enumerate(wordShapes):
-                features[(feature + str(i), shape)] = 1
+        '''
+        # VERY basic feature set for sanity check tests during development
+        for i,ind in enumerate(chunk_inds):
+            features = {('phrase',sentence[ind]) : 1} 
+            features_list[i] = features
+        return features_list
+        '''
 
-        if feature == "metric_unit":
-            unit = 0
-            if is_weight(word):
-                unit = 1
-            elif is_size(word):
-                unit = 2
-            features[(feature, None)] = unit
 
-        # look for prognosis locaiton
-        #if feature == "radial_loc":
-        # THIS MIGHT BE BUGGED
-        #    if is_prognosis_location(word):
-        #        features[(feature, None)] = 1
+        tags = []
 
-        if feature == "has_problem_form":
-            if has_problem_form(word):
-                features[(feature, None)] = 1
+        for i,ind in enumerate(chunk_inds):
 
-        if feature == "def_class":
-            features[(feature, None)] = get_def_class(word)
+            # Create a list of feature sets (one per chunk)
+            features = self.feat_sent.concept_features_for_chunk(sentence,ind)
 
-    return features
+            # Feature: Previous 3 POSs
+            if 'prev_3_pos' in self.feat_sent.enabled_concept_features:
+                tags = tags = nltk.pos_tag(sentence)
+                if ind == 0:
+                    prev_pos = ('*','*','*')
+                    features[('prev_3_pos',prev_pos)] = 1
+                elif ind == 1:
+                    prev_pos = ('*','*',tags[0][1])
+                    features[('prev_3_pos',prev_pos)] = 1
+                elif ind == 2:
+                    prev_pos = ('*',tags[0][1],tags[1][1])
+                    features[('prev_3_pos',prev_pos)] = 1
+                else:
+                    prev_pos = (tags[ind-3][1],tags[ind-2][1],tags[ind-1][1])
+                    features[('prev_3_pos',prev_pos)] = 1
 
-mitre_features = {
-    "INITCAP": r"^[A-Z].*$",
-    "ALLCAPS": r"^[A-Z]+$",
-    "CAPSMIX": r"^[A-Za-z]+$",
-    "HASDIGIT": r"^.*[0-9].*$",
-    "SINGLEDIGIT": r"^[0-9]$",
-    "DOUBLEDIGIT": r"^[0-9][0-9]$",
-    "FOURDIGITS": r"^[0-9][0-9][0-9][0-9]$",
-    "NATURALNUM": r"^[0-9]+$",
-    "REALNUM": r"^[0-9]+.[0-9]+$",
-    "ALPHANUM": r"^[0-9A-Za-z]+$",
-    "HASDASH": r"^.*-.*$",
-    "PUNCTUATION": r"^[^A-Za-z0-9]+$",
-    "PHONE1": r"^[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$",
-    "PHONE2": r"^[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$",
-    "FIVEDIGIT": r"^[0-9][0-9][0-9][0-9][0-9]",
-    "NOVOWELS": r"^[^AaEeIiOoUu]+$",
-    "HASDASHNUMALPHA": r"^.*[A-z].*-.*[0-9].*$ | *.[0-9].*-.*[0-9].*$",
-    "DATESEPERATOR": r"^[-/]$",
-}
 
-def is_test_result( context):
-    # note: make spaces optional?
-    regex = r"^[A-Za-z]+( )*(-|--|:|was|of|\*|>|<|more than|less than)( )*[0-9]+(%)*"
-    if not re.search(regex, context):
-        return re.search(r"^[A-Za-z]+ was (positive|negative)", context)
-    return True
+            # Feature: Previous Chunks's Features
+            if "prev" in self.feat_sent.enabled_concept_features:
+                if ind == 0:
+                    features[("prev", "*")] = 1
+                else:
+                    # Get features of previous chunks
+                    prev_features = self.feat_sent.concept_features_for_chunk(sentence,ind-1)
+                    prepend = lambda f: {("prev_"+k[0],k[1]):v for k, v in f.items()}
+                    features.update( prepend(prev_features) ) 
 
-def is_weight( word):
-    regex = r"^[0-9]*(mg|g|milligrams|grams)$"
-    return re.search(regex, word)
 
-def is_size( word):
-    regex = r"^[0-9]*(mm|cm|millimeters|centimeters)$"
-    return re.search(regex, word)
+            # Feature: Next Chunk's Features
+            if "next" in self.feat_sent.enabled_concept_features:
+                if ind == len(sentence) - 1:
+                    features[("next", "*")] = 1
+                else:
+                    # Get features of previous chunks
+                    next_features = self.feat_sent.concept_features_for_chunk(sentence,ind+1)
+                    prepend = lambda f: {("next_"+k[0],k[1]):v for k, v in f.items()}
+                    features.update( prepend(next_features) ) 
 
-def is_prognosis_location( word):
-    regex = r"^(c|C)[0-9]+(-(c|C)[0-9]+)*$"
-    return re.search(regex, word)
+            features_list[i] = features
 
-def has_problem_form( word):
-    regex = r".*(ic|is)$"
-    return re.search(regex, word)
 
-# checks for a definitive classification at the word level
-def get_def_class( word):
-    test_terms = {
-        "eval", "evaluation", "evaluations",
-        "sat", "sats", "saturation",
-        "exam", "exams",
-        "rate", "rates",
-        "test", "tests",
-        "xray", "xrays",
-        "screen", "screens",
-        "level", "levels",
-        "tox"
-    }
-    problem_terms = {
-        "swelling",
-        "wound", "wounds",
-        "symptom", "symptoms",
-        "shifts", "failure",
-        "insufficiency", "insufficiencies",
-        "mass", "masses",
-        "aneurysm", "aneurysms",
-        "ulcer", "ulcers",
-        "trama", "cancer",
-        "disease", "diseased",
-        "bacterial", "viral",
-        "syndrome", "syndromes",
-        "pain", "pains"
-        "burns", "burned",
-        "broken", "fractured"
-    }
-    treatment_terms = {
-        "therapy",
-        "replacement",
-        "anesthesia",
-        "supplement", "supplemental",
-        "vaccine", "vaccines"
-        "dose", "doses",
-        "shot", "shots",
-        "medication", "medicine",
-        "treament", "treatments"
-    }
-    if word.lower() in test_terms:
-        return 1
-    elif word.lower() in problem_terms:
-        return 2
-    elif word.lower() in treatment_terms:
-        return 3
-    return 0
+        return features_list
+
+
+
+    # prose_sentence()
+    #
+    # input:  A sentence
+    # output: Boolean yes/no
+    def prose_sentence(self, sentence):
+
+        # Empty sentence is not prose
+        if not sentence:
+            return False
+
+        if sentence[-1] == '.' or sentence[-1] == '?':
+            return True
+        elif sentence[-1] == ':':
+            return False
+        elif len(sentence) <= 5:
+            return False
+        elif self.at_least_half_nonprose(sentence):
+            return True
+        else:
+            return False
+
+
+    # at_least_half_nonprose()
+    #
+    # input:  A sentence
+    # output: A bollean yes/no
+    def at_least_half_nonprose(self, sentence):
+
+        count = 0
+
+        for word in sentence:
+            if self.prose_word(word):
+                count += 1
+
+        if count >= len(sentence):
+            return True
+        else:
+            return False
+
+
+    # prose_word()
+    #
+    # input:  A word
+    # output: Boolean yes/no
+    def prose_word(self, word):
+
+        # Punctuation
+        for punc in ".?,!:\"'":
+            if re.search(punc, word):
+                return False
+
+        # Digit
+        if re.match('\d', word):
+            return False
+
+        # All uppercase
+        if word == word.upper():
+            return False
+
+        # Else
+        return True
+
+
+
+
+# generate_chunks()
+#
+# input: Three arguments:
+#           1) A list of list of word (the data of the file)
+#           2) A list of list of IOB tags (one-to-one with list from arg 1)
+#           3) A list of list of concepts (one-to-one with list from arg 1)
+#
+#
+# output: A 3-tuple of:
+#           1) A list of list of chunks (word token phrases)
+#           2) A list of list of concepts (one-to-one with list from ret 1)
+#           3) A list of all indices into 1 of all non-'none' concepts
+def generate_chunks(data, IOB_tags, labels=None):
+
+    # List of list of tokens (similar to 'text', but concepts are grouped)
+    text_chunks   = []
+
+    # one-to-one concept classification with text_chunks
+    if labels:
+        concept_chunks = []
+    else:
+       concept_chunks = None
+
+    # List of (line,token) pairs for classifications that are nont 'none'
+    hits = []
+
+    # Create tokens of full concept boundaries for second classifier
+    for i, concept_line in enumerate(IOB_tags):
+
+        # One line of 'chunked'
+        line_of_text_chunks    = []
+        if labels: line_of_concept_chunks = []
+
+        # stores the current streak
+        queue = []
+
+        # Necessary when multiple concepts are on one line
+        # The second concept's j index is relative to a word-split array
+        # The j of the new token should be relative to how chunks there are
+        chunk_offset = 0
+
+        # C-style array indexing. Probably could be done a better way.
+        # Used because I needed the ability of lookahead
+        for j in range(len(concept_line)):
+
+            # Outside
+            # concept_line in "012" instead of "IOB"
+            if concept_line[j] == 'O':
+                line_of_text_chunks.append(data[i][j])
+                if labels: line_of_concept_chunks.append('none')
+
+            # Beginning of a concept boundary
+            else:
+
+                # Increase size of current streak
+                queue.append(data[i][j])
+
+                # lookahead (check if streak will continue)
+                if (j+1 == len(concept_line))or \
+                   (concept_line[j+1] != 'I'):     # end of classifiation
+
+                       # Add full concept token
+                       line_of_text_chunks.append(' '.join(queue))
+                       if labels: line_of_concept_chunks.append(labels[i][j])
+
+                       # Store indices of detected concept
+                       hits.append( ( i, j + 1-len(queue) - chunk_offset ) )
+
+                       # Reminder: used in the case that a concept follows a
+                       #           multi-word concept on the same line
+                       chunk_offset += len(queue) - 1
+
+                       # Reset streak
+                       queue = []
+
+        text_chunks.append(line_of_text_chunks)
+        if labels: concept_chunks.append(line_of_concept_chunks)
+
+
+    return (text_chunks, concept_chunks, hits)
+
