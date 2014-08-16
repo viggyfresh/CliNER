5c3b88b742cd83d61f3594657b63a290c3a9dd0a
ktwaco <kdubya12345@gmail.com>
2014-08-16 10:42:36 -0400

    merging scripting and cli

--- clicon/evaluate.py
+++ clicon/evaluate.py
@@ -84,15 +84,12 @@ def main():
             if k in con_files_map and k in ref_files_map:
                 files.append((txt_files_map[k], con_files_map[k], ref_files_map[k]))
 
-
         # Compute the confusion matrix
         labels = Model.labels   # hash tabble: label -> index
         confusion = [[0] * len(labels) for e in labels]
 
-
         print files
         print ''
-
 
         # txt <- medical text
         # con <- model predictions
@@ -107,12 +104,107 @@ def main():
             rnote = Note()
             rnote.read_i2b2( txt, ref )
 
-            # Get corresponding concept labels (prediction vs. actual)
-            for c, r in zip( cnote.conlist(), rnote.conlist() ):
-                for c, r in zip(c, r):
-                    confusion[labels[r]][labels[c]] += 1
+            predictions = cnote.conlist() 
 
+            #TO DO: make leaner
+            #TO DO: create unit tests 
+            reference_concept_spans = {} 
+            predicted_concept_spans = {} 
+           
+            for line_index, span in enumerate(rnote.boundaries):
+                #obtain each boundary for the reference labels. 
+                for boundary_index, boundary in enumerate(span):             
+                    #IF none/'O' for references simply add to confusion matrix.
+                    #this is done because a blank can only match or mismatch.
+                    if boundary == 'O':
+                        actual = labels[rnote.conlist()[line_index][boundary_index]]
+                        predicted = labels[cnote.conlist()[line_index][boundary_index]]
+                        confusion[actual][predicted] += 1
+                    #'B' signifies beginning of a concept. 
+                    if boundary == 'B': 
+                        concept = rnote.concepts[line_index][boundary_index]
+                        #create an entry for the line number for this span.  
+                        if reference_concept_spans.has_key(line_index) == False:
+                            reference_concept_spans[line_index] = {}
+                        #mark the beginning of the concept.  
+                        beginning = boundary_index 
+                        end = boundary_index                          
+                        for possible_end in span[boundary_index+1:]:
+                            #find the end of the concept 
+                            if possible_end == 'B' or possible_end == 'O': 
+                                break 
+                            if possible_end == 'I':
+                                end += 1  
+                        #store this concept to its line number. 
+                        reference_concept_spans[line_index].update({(beginning,end):concept})
+            
+            #find the labels generated from predictions.
+            #same logic as above except we only care about the labels created for predictions. 
+            for line_index, span in enumerate(cnote.boundaries):
+                for boundary_index, boundary in enumerate(span):
+                    if boundary == 'B':
+                        concept = cnote.concepts[line_index][boundary_index]
+                        beginning = boundary_index
+                        end = boundary_index
+                        if predicted_concept_spans.has_key(line_index) == False:
+                            predicted_concept_spans[line_index] = {}
+                        
+                        for possible_end in span[boundary_index+1:]:
+                            if possible_end == 'B' or possible_end == 'O':
+                                break
+                            if possible_end == 'I':
+                                end += 1
+                        predicted_concept_spans[line_index].update({(beginning,end):concept})
+            
+            #for each reference span verify if the predicted span corresponds.
+            #exatch matches     
+            for lines in reference_concept_spans:
+                spans_to_remove = []
+                for spans in reference_concept_spans[lines]:
+                    #if the line does not exist for whatever reason mark it as none. 
+                    if (lines in predicted_concept_spans) == False:
+                        confusion[labels[reference_concept_spans[lines][spans]]][labels['none']] += 1
+                        continue
+                    #if the span exists and the concept matches mark it for removal and add to confusion matrix
+                    if spans in predicted_concept_spans[lines]: 
+                       if reference_concept_spans[lines][spans] == predicted_concept_spans[lines][spans]: 
+                          #add to confusion matrix.
+                          spans_to_remove.append(spans)    
+                          confusion[labels[reference_concept_spans[lines][spans]]][labels[predicted_concept_spans[lines][spans]]] += 1 
+                for span_to_remove in spans_to_remove:
+                    #prevent double counting. 
+                    predicted_concept_spans[lines].pop(span_to_remove)
+                    reference_concept_spans[lines].pop(span_to_remove)
 
+            #these left overspans do not have exact matches. 
+            #for each reference span see if there is a predict span that it falls in the range of
+            #inexact matches
+            for lines in reference_concept_spans:
+                #if line does not exist for whatever reason just continue, these have already been marked for score. 
+                if (lines in predicted_concept_spans) == False:
+                    continue
+                for ref_spans in reference_concept_spans[lines]:
+                    incorrect_classifications = [] 
+                    for predicted_spans in predicted_concept_spans[lines]: 
+                        #find overlapping spans 
+                        if predicted_spans[0] >= ref_spans[0] and predicted_spans[0] <= ref_spans[1] \
+                           or predicted_spans[1] >= ref_spans[0] and predicted_spans[1] <= ref_spans[1]:
+                            #if the classification matches then mark it for score. 
+                            if reference_concept_spans[lines][ref_spans] == predicted_concept_spans[lines][predicted_spans]:
+                                confusion[labels[reference_concept_spans[lines][ref_spans]]][labels[predicted_concept_spans[lines][predicted_spans]]] += 1 
+                                incorrect_classifications = [] 
+                                break 
+                            #overlap but incorrect classification. we still store what it gets for now because there may be another overlap
+                            #that does match inexactly. 
+                            else: 
+                                incorrect_classifications.append(predicted_concept_spans[lines][predicted_spans])
+                    #if this is greater than 0 then there has been an incorrect classification. just map the first index
+                    #it is wrong either way. 
+                    if len(incorrect_classifications) > 0:
+                        confusion[labels[reference_concept_spans[lines][ref_spans]]][labels[incorrect_classifications[0]]] += 1
+                    #if there is no predicted span that overlaps with the reference span just mark as none.  
+                    else:
+                        confusion[labels[reference_concept_spans[lines][ref_spans]]][labels['none']] += 1
 
         # Display the confusion matrix
         print >>args.output, ""
@@ -129,8 +221,6 @@ def main():
             print >>args.output, "%s %s" % (act.rjust(pad), "\t".join([str(confusion[act_v][pre_v]) for pre, pre_v in labels.items()]))
         print >>args.output, ""
 
-
-
         # Compute the analysis stuff
         precision = []
         recall = []
@@ -144,8 +234,6 @@ def main():
 
         print >>args.output, "Analysis"
         print >>args.output, " " * pad, "Precision\tRecall\tF1"
-
-
 
         for lab, lab_v in labels.items():
             tp = confusion[lab_v][lab_v]
