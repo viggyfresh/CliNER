f8b589ecfc9bad39bce77324c3860eb58dfa2322
Stuart Tomkins <stomkins@dumuzi.(none)>
2015-03-23 15:55:19 -0400

    added more doctests and general documentation

--- cliner/features_dir/word_features.py
+++ cliner/features_dir/word_features.py
@@ -35,12 +35,15 @@ class WordFeatures:
         pass
 
 
-    # IOB_prose_features_for_word()
-    #
-    # input:  A single word
-    # output: A dictionary of features
     def IOB_prose_features(self, word):
-
+        """
+        IOB_prose_features()
+        
+        Purpose: Creates a dictionary of prose  features for the given word.
+        
+        @param word. A string
+        @return      A dictionary of features
+        """
         # Feature: <dummy>
         features = {('dummy', None): 1}  # always have >0 dimensions
   
@@ -86,15 +89,15 @@ class WordFeatures:
         return features
 
 
-
-
-
-    # IOB_nonprose_features_for_word()
-    #
-    # input:  A single word
-    # output: A dictionary of features
     def IOB_nonprose_features(self, word):
-
+        """
+        IOB_nonprose_features()
+        
+        Purpose: Creates a dictionary of nonprose features for the given word.
+        
+        @param word. A string
+        @return      A dictionary of features
+        """
         features = {}
 
         # Feature: The word, itself
@@ -129,6 +132,8 @@ class WordFeatures:
 
         """
         concept_features_for_word()
+
+        Purpose: Creates a dictionary of concept features for the given word.
  
         @param  word. A word to generate features for
         @return       A dictionary of features
@@ -210,8 +215,8 @@ class WordFeatures:
         return features
 
 
-
-
+    #FIXME
+    # The documentation for this is incorrect.
     def concept_features_for_chunk(self, sentence, ind):
 
         """
@@ -286,6 +291,14 @@ class WordFeatures:
 
     # Try to get QANN features
     def QANN_features(self, word):
+        """
+        QANN_features()
+
+        Purpose: Creates a dictionary of QANN features for the given word. 
+
+        @param word. A string
+        @return      A dictionary of features
+        """
         features = {}
 
         # Feature: test result
@@ -320,19 +333,81 @@ class WordFeatures:
 
         return features
 
-
+    # note: make spaces optional?
     def is_test_result(self, context):
-        # note: make spaces optional?
+        """
+        is_test_result()
+
+        Purpose: Checks if the context is a test result.
+
+        @param context. A string.
+        @return         it returns the matching object of '[blank] was positive/negative' or None if it cannot find it.
+                        otherwise, it will return True.
+        """
         regex = r"^[A-Za-z]+( )*(-|--|:|was|of|\*|>|<|more than|less than)( )*[0-9]+(%)*"
         if not re.search(regex, context):
             return re.search(r"^[A-Za-z]+ was (positive|negative)", context)
         return True
 
     def is_measurement(self, word):
+        """
+        is_measurement()
+
+        Purpose: Checks if the word is a measurement.
+
+        @param word. A string.
+        @return      the matched object if it is a measurement, otherwise None.
+
+        >>> wf = WordFeatures()
+        >>> wf.is_measurement('1 unit') is not None
+        True
+        >>> wf.is_measurement('7 units') is not None
+        True
+        >>> wf.is_measurement('10cc') is not None
+        True
+        >>> wf.is_measurement('300 L') is not None
+        True
+        >>> wf.is_measurement('20ml') is not None
+        True
+        >>> wf.is_measurement('400000 dL') is not None
+        True
+        >>> wf.is_measurement('30000') is not None
+        False
+        >>> wf.is_measurement('20dl') is not None
+        False
+        >>> wf.is_measurement('units') is not None
+        True
+        """
         regex = r"^[0-9]*(unit(s)|cc|L|mL|dL)$"
         return re.search(regex, word)
 
     def is_directive(self, word):
+        """
+        is_directive()
+
+        Purpose: Checks if the word is a directive.
+
+        @param word. A string.
+        @return      the matched object if it is a directive, otherwise None.
+
+        >>> wf = WordFeatures()
+        >>> wf.is_directive('q.abc') is not None
+        True
+        >>> wf.is_directive('qAD') is not None 
+        True
+        >>> wf.is_directive('PRM') is not None 
+        True
+        >>> wf.is_directive('bid') is not None 
+        True
+        >>> wf.is_directive('prm') is not None 
+        True
+        >>> wf.is_directive('p.abc') is not None 
+        True
+        >>> wf.is_directive('qABCD') is not None 
+        False
+        >>> wf.is_directive('BID') is not None 
+        False
+        """
         regex = r"^(q\..*|q..|PRM|bid|prm|p\..*)$"
         return re.search(regex, word)
 
@@ -343,45 +418,160 @@ class WordFeatures:
         Purpose: Checks if word is a date.
 
         @param word. A string.
-        @return the matched object if it is a date, otherwise None.
+        @return      the matched object if it is a date, otherwise None.
 
         >>> wf = WordFeatures()
-        >>> wf.is_date("2015-03-1") is not None
+        >>> wf.is_date('2015-03-1') is not None
         True
-        >>> wf.is_date("2014-02-19") is not None
+        >>> wf.is_date('2014-02-19') is not None
         True
-        >>> wf.is_date("03-27-1995") is not None
-        True    
-        >>> wf.is_date("201") is not None
+        >>> wf.is_date('03-27-1995') is not None
+        True
+        >>> wf.is_date('201') is not None
         False
-        >>> wf.is_date("0") is not None
-        False   
+        >>> wf.is_date('0') is not None
+        False
         """
         regex= r'^(\d\d\d\d-\d\d-\d|\d\d?-\d\d?-\d\d\d\d?|\d\d\d\d-\d\d?-\d\d?)$'
         return re.search(regex,word)
 
     def is_volume(self, word):
+        """
+        is_volume()
+
+        Purpose: Checks if word is a volume. 
+
+        @param word. A string.
+        @return      the matched object if it is a volume, otherwise None.
+
+        >>> wf = WordFeatures()
+        >>> wf.is_volume('9ml') is not None
+        True
+        >>> wf.is_volume('10 mL') is not None
+        True
+        >>> wf.is_volume('552 dL') is not None
+        True
+        >>> wf.is_volume('73') is not None
+        False
+        >>> wf.is_volume('ml') is not None
+        True
+        """
         regex = r"^[0-9]*(ml|mL|dL)$"
         return re.search(regex, word)
 
     def is_weight(self, word):
+        """
+        is_weight()
+
+        Purpose: Checks if word is a weight.
+
+        @param word. A string.
+        @return      the matched object if it is a weight, otherwise None.
+
+        >>> wf = WordFeatures()
+        >>> wf.is_weight('1mg') is not None
+        True
+        >>> wf.is_weight('10 g') is not None 
+        True
+        >>> wf.is_weight('78 mcg') is not None  
+        True
+        >>> wf.is_weight('10000 milligrams') is not None  
+        True
+        >>> wf.is_weight('14 grams') is not None  
+        True
+        >>> wf.is_weight('-10 g') is not None  
+        False
+        >>> wf.is_weight('grams') is not None
+        True
+        """
         regex = r"^[0-9]*(mg|g|mcg|milligrams|grams)$"
         return re.search(regex, word)
 
     def is_size(self, word):
+        """
+        is_size()
+
+        Purpose: Checks if the word is a size.
+
+        @param word. A string.
+        @return      the matched object if it is a weight, otheriwse None.
+
+        >>> wf = WordFeatures()
+        >>> wf.is_size('1mm') is not None
+        True
+        >>> wf.is_size('10 cm') is not None   
+        True
+        >>> wf.is_size('36 millimeters ') is not None   
+        True
+        >>> wf.is_size('423 centimeters') is not None   
+        True
+        >>> wf.is_size('328') is not None   
+        False
+        >>> wf.is_size('22 meters') is not None   
+        False
+        >>> wf.is_size('millimeters') is not None  
+        True
+        """
         regex = r"^[0-9]*(mm|cm|millimeters|centimeters)$"
         return re.search(regex, word)
 
     def is_prognosis_location(self, word):
+        """
+        is_prognosis_location()
+
+        Purpose: Checks if the word is a prognosis location
+
+        @param word. A string.
+        @return      the matched object if it is a prognosis location, otherwise None.
+        """
         regex = r"^(c|C)[0-9]+(-(c|C)[0-9]+)*$"
         return re.search(regex, word)
 
     def has_problem_form(self, word):
+        """
+        has_problem_form()
+
+        Purpose: Checks if the word has problem form.
+
+        @param word. A string
+        @return      the matched object if it has problem form, otheriwse None.
+        """
         regex = r".*(ic|is)$"
         return re.search(regex, word)
 
-    # checks for a definitive classification at the word level
     def get_def_class(self, word):
+        """
+        get_def_class()
+
+        Purpose: Checks for a definitive classification at the word level.
+
+        @param word. A string
+        @return      1 if the word is a test term,
+                     2 if the word is a problem term,
+                     3 if the word is a treatment term,
+                     0 otherwise.
+        >>> wf = WordFeatures();
+        >>> wf.get_def_class('eval')
+        1
+        >>> wf.get_def_class('rate') 
+        1
+        >>> wf.get_def_class('tox') 
+        1
+        >>> wf.get_def_class('swelling') 
+        2
+        >>> wf.get_def_class('mass') 
+        2
+        >>> wf.get_def_class('broken') 
+        2
+        >>> wf.get_def_class('therapy') 
+        3
+        >>> wf.get_def_class('vaccine') 
+        3
+        >>> wf.get_def_class('treatment') 
+        3
+        >>> wf.get_def_class('unrelated') 
+        0
+        """ 
         test_terms = {
             "eval", "evaluation", "evaluations",
             "sat", "sats", "saturation",
@@ -419,7 +609,7 @@ class WordFeatures:
             "dose", "doses",
             "shot", "shots",
             "medication", "medicine",
-            "treament", "treatments"
+            "treatment", "treatments"
         }
         if word.lower() in test_terms:
             return 1
