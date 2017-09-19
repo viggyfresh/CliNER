0bdd466cbf9d84a4868019102e74e66a273c8116
Sim Dy <simthyrearch_dy@student.uml.edu>
2017-09-19 09:26:02 -0400

    Updated information about lstm feature support

--- code/train.py
+++ code/train.py
@@ -76,6 +76,10 @@ def main():
         print >>sys.stderr, '\n\tError: Model dir does not exist: %s' % modeldir
         print >>sys.stderr,  ''
         exit(1)
+    if args.use_lstm:
+	print >>sys.stderr, '\n\t --use-lstm not supported yet'
+	print >>sys.stderr, ''
+	exit(1)
 
     # A list of txt and concept file paths
     txt_files = glob.glob(args.txt)
